ed7456f82da2d1d440ff90a36c5196401c32c847
simranjit-kaur822 <simikaur.work@gmail.com>
2025-11-23 18:19:11 +0530

    create generate password function

--- passGenerator/src/App.jsx
+++ passGenerator/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
-
+import { useState, useCallback } from 'react'
 
 function App() {
   
-  const [length, setLength] = useState(8)
-  const [number, setNumber] = useState(false)
-  const [character, setCharacter] = useState(false)
-  const [password, setPassword] = useState('')
+  // States - length, numberAllowed, characterAllowed, password
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState('');
+
+  // Function to generate password
+  const passwordGenerator = useCallback(() =>{
+    let pass = '';
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const characters = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+    if(numberAllowed) letters += numbers;
+    if(characterAllowed) letters += characters;
+
+    for(let i = 0; i <length; i++){
+      let char = Math.floor(Math.random() * letters.length + 1);
+      pass += letters.charAt(char);
+    }
+
+    setPassword(pass);
+  },[length,numberAllowed,characterAllowed, setPassword])
+
 
   return (
     <>
